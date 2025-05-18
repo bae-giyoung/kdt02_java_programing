3a4d41ceb7f4ed07eb4ff9a1a86d00670ffef738
giyou <giyou@giyoung1>
2025-05-18 23:20:20 +0900

    Java 문자열 연습

---
+++ practice.note/day1/P3.java
@@ -0,0 +1,47 @@
+package day1;
+
+import java.util.Scanner;
+
+public class P3 {
+
+	public static void main(String[] args) {
+		// 입력받은 문자열의 대소문자 변경하기
+		
+		// 방법1: String은 불변객체이기 때문에 밑의 코드는 새로운 객체를 계속 생성한다. 안 좋은 코드로 생각. 그렇기 때문에 방법2로 풀어보자!
+		Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String answer = "";
+        
+        for (int i = 0; i < a.length(); i++) {
+        	String s = String.valueOf(a.charAt(i));
+        	
+            if (a.charAt(i) >= 'a' && a.charAt(i) <= 'z')
+                answer += s.toUpperCase();
+            else if (a.charAt(i) >= 'A' && a.charAt(i) <= 'Z')
+                answer += s.toLowerCase();
+            else
+            	answer += s;
+        }
+        
+        System.out.println(answer);
+        System.out.println();
+        
+        
+        // 방법2
+        for (int i = 0; i < a.length(); i++) {
+        	char c = a.charAt(i);
+            if (Character.isLowerCase(c))
+                System.out.print((char)(c-32));
+            else if (Character.isUpperCase(c))
+            	System.out.print((char)(c+32));
+            else
+            	System.out.print(c);
+        }
+        
+        System.out.println();
+        
+        // 방법3 : Arrays API 학습 후, StringBuilder, StringBuffer로 가변객체 학습한 후 풀어보기!
+
+	}
+
+}
